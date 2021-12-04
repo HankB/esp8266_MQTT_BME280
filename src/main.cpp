182b71b4faa3ee7d6e2861dd557e775b4d146251
Hank Barta <hbarta@gmail.com>
2021-12-03 22:08:34 -0600

    rambling/planning next feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 /*
- Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
+ Basic ESP8266 MQTT example ... now heavily modified.
+ - Associate with AP and connect to MQTT broker.
+ - Subscribe to a message that will 
+    1) provide a timestamp and 2) trigger 
+    2) trigger publication of conditions read from the BME280
+ - When the message arrives, extract the timestamp, read the BME280 
+   and publish the results (in the callback)
+
+ Alternatives
+  Subscribe to an NTP server and be independant of the other
+  process publishing the message.
 */
 
 #include <ESP8266WiFi.h>
